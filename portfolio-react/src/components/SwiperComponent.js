cae11b0c73e79798a9b28dfe582882e10396a40b
Dylan Schoop <42898108+CurleyDS@users.noreply.github.com>
2024-07-18 13:41:25 +0200

    Added project_links

--- portfolio-react/src/components/SwiperComponent.js
+++ portfolio-react/src/components/SwiperComponent.js
@@ -28,10 +28,16 @@ function Slides({ data = [] }) {
             {data.map((item, index) => (
                 <SwiperSlide key={index} className='flex flex-col justify-center'>
                     <div className='flex flex-col justify-center p-5'>
-                        {item.img && <img src={item.img} alt={item.title} className='relative object-scale-down h-64 mx-auto p-5' />}
+                        {item.img && (item.project_link ? (
+                            <a href={item.project_link} target="_blank" rel="noopener noreferrer" title='Go to Project'>
+                            <img src={item.img} alt={item.title} className='relative object-scale-down h-64 mx-auto p-5' />
+                        </a>
+                        ) : (
+                            <img src={item.img} alt={item.title} className='relative object-scale-down h-64 mx-auto p-5' />
+                        ))}
                         {item.title && <h3 className='absolute left-0 bottom-0 p-5'>{item.title}</h3>}
-                        {item.link && <a href={item.link} target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon icon={faGithub} className='absolute right-0 bottom-0 p-5' title='Go to Github Project' size='2x' />
+                        {item.github_link && <a href={item.github_link} target="_blank" rel="noopener noreferrer" title='Go to Github Project'>
+                            <FontAwesomeIcon icon={faGithub} className='absolute right-0 bottom-0 p-5' size='2x' />
                         </a>}
                     </div>
                 </SwiperSlide>
